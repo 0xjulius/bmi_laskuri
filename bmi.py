2aac4e435ca2d337471bb7a64185f62909de3965
JULIUS AALTO <72438387+0xjulius@users.noreply.github.com>
2024-02-20 21:21:31 +0200

    Update bmi.py
    *added while loop
    *added user input validation
    *changed BMI formula

--- bmi.py
+++ bmi.py
@@ -1,34 +1,51 @@
 import math
+
 nimi = input("Mikä on etunimesi?: ")
 print("Hei " + nimi.capitalize() + "!")
-pituus = float(input("Kuinka pitkä olet?: "))
-paino = float(input("Kuinka painava olet?: "))
 
-s_pituus = str(pituus)
-s_paino = str(paino)
+while True:
+    try:
+        pituus_str = input("Kuinka pitkä olet?: ").replace(",", ".")
+        pituus = float(pituus_str)
+        if pituus <= 0:
+            print("Syötä positiivinen numero")
+        else:
+            break
+    except ValueError:
+        print("Ole hyvä ja syötä numeerinen arvo!")
+
+while True:
+    try:
+        paino_str = input("Kuinka painava olet?: ").replace(",", ".")
+        paino = float(paino_str)
+        if paino <= 0:
+            print("Syötä positiivinen numero")
+        else:
+            break
+    except ValueError:
+        print("Ole hyvä ja syötä numeerinen arvo!")
 
 pituusM = pituus / 100
-pituuspow = math.pow(pituusM, 2.5)
-bmi = 1.3 * paino/pituuspow
+bmi = paino / (pituusM * pituusM)
 
-print("Pituutesi on " + s_pituus + " senttimetriä.")
-print("Painosi on " + s_paino + " kiloa.")
+print("Pituutesi on", pituus, "senttimetriä.")
+print("Painosi on", paino, "kiloa.")
 print("Painoindeksisi on: {:.2f}".format(bmi))
 
-if(bmi>0):
-    if(bmi<=14.9):
+if bmi > 0:
+    if bmi <= 14.9:
         print("Olet sairaalloisen alipainoinen! ")
-    elif(bmi<=17.9):
+    elif bmi <= 17.9:
         print("Merkittävä alipaino! ")
-    elif(bmi<=18.9):
+    elif bmi <= 18.9:
         print("Lievä alipaino. ")
-    elif(bmi <= 24.9):
+    elif bmi <= 24.9:
         print("Onnittelut! Olet normaalipainoinen.")
-    elif(bmi<=29.9):
+    elif bmi <= 29.9:
         print("Lievä lihavuus. ")
-    elif(bmi <= 34.9):
+    elif bmi <= 34.9:
         print("Merkittävä ylipaino. ")
-    elif(bmi <= 39.9):
+    elif bmi <= 39.9:
         print("Vaikea ylipaino. ")
     else:
         print("Sairaalloinen ylipaino. ")
